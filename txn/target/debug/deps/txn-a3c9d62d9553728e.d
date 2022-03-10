2cad1754705ab33c2662df00d29f82cf09efedee
dust-where <1337872545@qq.com>
2022-03-10 16:49:32 +0800

    3-10

---
+++ txn/target/debug/deps/txn-a3c9d62d9553728e.d
@@ -0,0 +1,5 @@
+/home/dust-where/txn/txn/txn/target/debug/deps/txn-a3c9d62d9553728e: src/main.rs
+
+/home/dust-where/txn/txn/txn/target/debug/deps/txn-a3c9d62d9553728e.d: src/main.rs
+
+src/main.rs:
---
+++ txn/target/debug/txn.d
@@ -0,0 +1,1 @@
+/home/dust-where/txn/txn/txn/target/debug/txn: /home/dust-where/txn/txn/txn/src/main.rs
